4b9a55b9fb845ea709922258e1fad715d039778e
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-19 18:53:52 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://docbooker-backend-fpxs.onrender.com/api/v1/user/login",
         { email, password, confirmPassword, role: "Admin" },
         {
           withCredentials: true,
