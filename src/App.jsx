7dc32fe98acbc07db52b071eb017013f17d8148a
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-19 18:55:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me", 
+          "https://docbooker-backend-fpxs.onrender.com/api/v1/user/admin/me", 
           { withCredentials: true }
         );
         setIsAuthenticated(true);
