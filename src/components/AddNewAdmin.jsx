7ad3639320c0046fc93cba9aad8bfd8e2a1b7f47
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-19 18:49:35 +0530

    Update AddNewAdmin.jsx

--- src/components/AddNewAdmin.jsx
+++ src/components/AddNewAdmin.jsx
@@ -24,7 +24,7 @@ const AddNewAdmin = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/v1/user/admin/addnew",
+        "https://docbooker-backend-fpxs.onrender.com/api/v1/user/admin/addnew",
         {
           firstName,
           lastName,
