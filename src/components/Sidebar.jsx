270eb59531056410ac19b8beb13d7872a8d61a82
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-20 17:54:47 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -45,7 +45,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("http://localhost:4000/api/v1/user/admin/logout", {
+      .get("https://docbooker-backend-2.onrender.com/api/v1/user/admin/logout", {
         withCredentials: true
       })
       .then((res) => {
